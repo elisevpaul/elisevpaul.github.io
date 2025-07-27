85edc30292d75997bca62dfe3762e7c4e4042b0c
Eilse Paul <epaul@datalot.com>
2025-07-27 15:02:53 -0400

    round two , timeline is showing and dom is fixed, reducing complexity

--- resources/js/articles/1.js
+++ resources/js/articles/1.js
@@ -1,22 +1,28 @@
 
-var data = [1995, 2000, 2005, 2010, 2015, 2020];
+  var article_1 = function() {
+   console.log("article_1");
+    var data = [1995, 2000, 2005, 2010, 2015, 2020];
 
-var width = 800;
-var height = 100;
-var timeline_scale = d3.scaleLinear()
-  .domain([1995, 2020])
-  .range([0, width]);
-  
-// Create a simple timeline using D3.js
-var tl = d3.select(".timeline")
-  .data(data)
-  .enter()
-  .append("p")
- .html(function(d) {
-	return "<span style='position: absolute; left: " + timeline_scale(d) + "px;'>" + d + "</span>";
+    var width = 800;
+    var height = 100;
+    var timeline_scale = d3.scaleLinear()
+      .domain([1995, 2020])
+      .range([0, width]);
+      
+    //Create a simple timeline using D3.js
+    var tl = d3.select(".timeline")
+      .selectAll("text")
+      .data(data)
+      .enter()
+      .append("text")
+      .attr("x", function(d) { return timeline_scale(d); })
+      .attr("y", 30)
+      .text(function(d) { return d; });
+
+    console.log(tl);
+    console.log(timeline_scale);
+    console.log(data);
+  }
+  document.addEventListener("DOMContentLoaded", function() {
+    article_1();
   });
-
-
-console.log(tl);
-console.log(timeline_scale);
-console.log(data);
