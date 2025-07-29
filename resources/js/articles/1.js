0784584e28dff441c17f112f5c889435101155ad
Eilse Paul <epaul@datalot.com>
2025-07-29 08:09:29 -0400

    working carousel for slides

--- resources/js/articles/1.js
+++ resources/js/articles/1.js
@@ -1,28 +1,54 @@
+var dataset_map = {
+  'main': '',
+  'traffic delay': 'annual_person_hours_of_traffic_delay_per_commuter.csv',
+  'operation costs': '',
+  'fuel waste': ''
+}
 
-  var article_1 = function() {
-   console.log("article_1");
-    var data = [1995, 2000, 2005, 2010, 2015, 2020];
 
-    var width = 800;
-    var height = 100;
-    var timeline_scale = d3.scaleLinear()
-      .domain([1995, 2020])
-      .range([0, width]);
-      
-    //Create a simple timeline using D3.js
-    var tl = d3.select(".timeline")
-      .selectAll("text")
-      .data(data)
-      .enter()
-      .append("text")
-      .attr("x", function(d) { return timeline_scale(d); })
-      .attr("y", 30)
-      .text(function(d) { return d; });
+var show_slide = function(slide_id) {
+  console.log("showing slide " + slide_id);
+  const new_slide = document.getElementById("slide_" + slide_id);
+  const slides = document.querySelectorAll('.slide');
+  Array.from(slides).forEach(slide=>{
+    slide.style.display = 'none';
+  })
 
-    console.log(tl);
-    console.log(timeline_scale);
-    console.log(data);
-  }
-  document.addEventListener("DOMContentLoaded", function() {
-    article_1();
-  });
+  new_slide.style.display = 'block';
+};
+
+
+var init_event_triggers = function() {
+  const slide_buttons = document.querySelectorAll('.slide_button');
+  Array.from(slide_buttons).forEach((slide_button, index)=>{
+    slide_button.addEventListener("click", function() {
+      show_slide(index);
+    });
+  })
+};
+
+var draw_visualizations = function() {
+  console.log("drawing visualizations with d3");
+  // loop through our dataset map.
+  // draw a graph based on the file with the same name in resources/datasets
+  // use d3 for all illustrations
+  // dynamically guess graph type based on dataset.
+};
+
+
+var add_annotations = function() {
+  console.log("adding additional info");
+  //add annotations to the graphs for additional info.
+};
+
+
+
+var load_page = function() {
+  show_slide(0 );
+  draw_visualizations();
+  add_annotations();
+  init_event_triggers();
+}
+document.addEventListener("DOMContentLoaded", function() {
+  load_page();
+});
